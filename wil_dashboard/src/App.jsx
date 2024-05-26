bdc38756db1e487a4efa227341725fa1384db390
m3lon4rt <m3lon0337@gmail.com>
2024-05-26 15:38:52 +0800

    Fix routing

--- wil_dashboard/src/App.jsx
+++ wil_dashboard/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={ <LoginPage/> }/>
-        <Route path="/MainPage" element={ <MainPage/> }/>
+        <Route path="/" element={ <MainPage/> }/>
       </Routes>
     </Router>
   )
