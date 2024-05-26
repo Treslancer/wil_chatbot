dfa7dca400a50cd3f86959c56354e99fa99a0068
m3lon4rt <m3lon0337@gmail.com>
2024-05-26 15:46:57 +0800

    Fix ruting

--- wil_dashboard/src/App.jsx
+++ wil_dashboard/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={ <LoginPage/> }/>
-        <Route path="/" element={ <MainPage/> }/>
+        <Route path="/MainPage" element={ <MainPage/> }/>
       </Routes>
     </Router>
   )
